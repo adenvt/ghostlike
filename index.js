b3e63526aa5a2badc9464d60f47ff8099830baf1
Ade Novid <adenvt@gmail.com>
2017-09-22 11:27:27 +0700

    v0.2.0
    * Saving cookies

--- index.js
+++ index.js
@@ -1,29 +1,87 @@
-const Nightmare = require('nightmare');
-const config = require('./config.json');
+const fs = require('fs')
+const Nightmare = require('nightmare')
+const CronJob = require('cron').CronJob;
+const json = require('jsonfile')
+const config = require('./config.json')
 
-Nightmare()
-  .goto('https://m.facebook.com')
-  .type('[name="email"]', config.email)
-  .type('[name="pass"]', config.pass)
-  .click('[name="login"]')
-  .wait('[value="OK"]')
-  .click('[value="OK"]')
-  .wait('a[data-sigil~="ufi-inline-like"]')
-  .evaluate(() => {
-    var btns = document.querySelectorAll('a[data-sigil~="ufi-inline-like"]');
+const nightmare = Nightmare()
 
-    btns.forEach(btn => {
-      var data = JSON.parse(btn.dataset.store);
-      if (data.reaction==0)
-        btn.click();
-    });
+function login () {
+  return new Promise((resolve, reject) => {
+    nightmare
+      .goto('https://m.facebook.com')
+      .type('[name="email"]', config.email)
+      .type('[name="pass"]', config.pass)
+      .click('[name="login"]')
+      .wait(2000)
+      .cookies.get()
+      .then(cookies => {
+        json.writeFile('cookies/cookies.json', cookies, err => {
+          if (err)
+            return reject(err)
 
-    return btns;
+          return resolve(cookies)
+        })
+      })
   })
-  .end()
-  .then(res => {
-    console.log('Success', Object.keys(res).length, 'likes sent');
+}
+
+function loadCookies() {
+  return new Promise((resolve, reject) => {
+    json.readFile('cookies/cookies.json', (err, cookies) => {
+      if (err)
+        return reject(err)
+
+      return resolve(cookies)
+    })
   })
-  .catch(err => {
-    console.error('Error', err);
+}
+
+function main() {
+  const promise = (fs.existsSync('cookies/cookies.json'))
+    ? loadCookies()
+    : login()
+
+  promise.then((cookies) => {
+    nightmare
+      .goto('https://m.facebook.com')
+      .cookies.set(cookies)
+      .then(() => {
+        const cron = new CronJob({
+          cronTime: '*/5 * * * * *',
+          timeZone: 'Asia/Jakarta',
+          onTick() {
+            nightmare
+              .refresh()
+              .wait('a[data-sigil~="ufi-inline-like"]')
+              .evaluate(() => {
+                var btns = document.querySelectorAll('a[data-sigil~="ufi-inline-like"]')
+                var counter = 0
+
+                btns.forEach(btn => {
+                  var data = JSON.parse(btn.dataset.store)
+
+                  if (data.reaction==0) {
+                    btn.click()
+
+                    ++counter
+                  }
+                })
+
+                return counter
+              })
+              .then(res => {
+                console.log('Success', Object.keys(res).length, 'likes sent')
+              })
+              .catch(err => {
+                console.error('Error', err)
+              })
+          }
+        })
+
+        cron.start()
+      })
   })
+}
+
+main()
